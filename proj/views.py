7bbaa2a20c3825eec19d914defcb72bb52d69a86
aom19 <39848284+aom19@users.noreply.github.com>
2023-03-26 18:37:25 +0300

    edit settings

--- proj/views.py
+++ proj/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+from . import tasks
+
+def home(request):
+    tasks.download_a_cat.delay()
+    return HttpResponse('<h1> HI THERE !!!!</h1>')
